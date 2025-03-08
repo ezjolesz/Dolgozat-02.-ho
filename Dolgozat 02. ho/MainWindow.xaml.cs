692f7cb19cb64efab6b18170138ea44f32d9777f
ezjolesz <toth.kende06@gmail.com>
2025-03-08 17:14:36 +0100

    Projekt létrehozva, fájlbeolvasás implementálva

--- Dolgozat 02. ho/MainWindow.xaml.cs
+++ Dolgozat 02. ho/MainWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Dolgozat_02._ho
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string[]> adatok = new List<string[]>();
+
         public MainWindow()
         {
             InitializeComponent();
+            Beolvasas();
+        }
+
+        private void Beolvasas()
+        {
+            foreach (var sor in File.ReadAllLines("jeladas.txt"))
+            {
+                var adatokSor = sor.Split('\t');
+                adatok.Add(adatokSor);
+            }
         }
     }
 }
